d30d5612d07197e48429a8fc84533ed40869cf74
harriet.ballantyne <harriet.ballantyne@gmail.com>
2019-03-26 14:05:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 def prime?(number)
   x = 2
   while x <= number / 2
-  if number % x = 0
-    false
-    x += 1
-  else
-    true
+    if number % x = 0
+      false
+      x += 1
+      else
+      true
+    end
   end
-end
 end
